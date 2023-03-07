b2746933d00ece12c2706d3473f03e54ec58eaef
Ray0v0 <947609598@qq.com>
2023-03-07 11:39:05 +0800

    a new Function

--- src/HelloWord.java
+++ src/HelloWord.java
@@ -2,5 +2,6 @@ public class HelloWord {
     public static void main(String[] args) {
         System.out.println("Hello Word, my name is Ray0v0");
         System.out.println("One bug has been fixed.");
+        System.out.println("This is a new function");
     }
 }
